b77df8841d81c1706bcceed2888c62941375d47f
Pawel Reich <thegitman@outlook.com>
2026-01-25 23:44:30 +0100

    scripts: Fix absolute disk.bin path

--- scripts/qemu.sh
+++ scripts/qemu.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env sh
-qemu-system-i386 -drive file=/IdeaProjects/GitOS/cmake-build-debug/disk.bin,format=raw,index=0,media=disk \
+qemu-system-i386 -drive file=./build/disk.bin,format=raw,index=0,media=disk \
                  -m 32M \
                  -chardev stdio,id=char0,logfile=serial.log,signal=off \
                  -serial chardev:char0
