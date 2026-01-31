6be49a89f1320a51e2a5be08ee958e4aba246546
Pawel Reich <thegitman@outlook.com>
2026-01-31 16:11:15 +0100

    [#12] stdlib: Add sleep function

--- src/userland/stdlib/misc.h
+++ src/userland/stdlib/misc.h
@@ -6,10 +6,19 @@
 
 #include <stdint.h>
 
-void execprocess(const char* path);
-void get_process_arguments(int* argc, char*** argv);
-void exit(int status);
-void* malloc(int size);
-void free(void* address);
-void open_ipc(const char* filename, uint32_t packet_size, uint32_t count);
-int getpid();
+#ifdef __cplusplus
+extern "C"
+{
+#endif
+    void execprocess(const char* path);
+    void get_process_arguments(int* argc, char*** argv);
+    void exit(int status);
+    void* malloc(int size);
+    void free(void* address);
+    void open_ipc(const char* filename, uint32_t packet_size, uint32_t count);
+    int getpid();
+    void gettick(uint64_t* val);
+    void sleep(uint64_t ms);
+#ifdef __cplusplus
+}
+#endif
